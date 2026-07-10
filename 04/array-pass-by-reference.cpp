e017ef907de96353fd034776f914df5cd8211f78
Muhammad Muzzamal <ashrafmuzzamal7@gmail.com>
2026-07-10 18:39:08 +0500

    arrays are always pass by reference

---
+++ 04/array-pass-by-reference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr)/sizeof(int);
+
+    printArr(arr, size);
+
+    return 0;
+}
